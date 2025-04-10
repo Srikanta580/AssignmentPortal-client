0ef47ad2f00ba5e390aee927ae00d40ae444a740
Anirban Dutta <meanirbandutta22@gmail.com>
2025-04-10 21:36:16 +0530

    test - api

--- src/pages/admin/Settings.jsx
+++ src/pages/admin/Settings.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import apiClient from "../../services/apiClient";
 
 function SettingsPage() {
-  return <div>SettingsPage</div>;
+  const [message, setMessage] = useState("");
+
+  const handleClick = async () => {
+    try {
+      const response = await apiClient.get("/");
+      setMessage(response.data);
+    } catch (error) {
+      setMessage("API call failed!");
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <div>
+      SettingsPage
+      <br />
+      <button onClick={handleClick}>Test API</button>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default SettingsPage;
